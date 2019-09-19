2dc5abf60ea348aec2b9e0391a93e3c4adb79520
KoreanWienerSchnizel <joeheart1@gmail.com>
2019-09-18 17:56:38 -0700

    modify dataprep

--- MahjongDataPrep.py
+++ MahjongDataPrep.py
@@ -1,4 +1,6 @@
 import csv
+import random
+
 import MahjongKit as mjkit
 import MahjongAgent as mjagent
 from os import path
@@ -8,9 +10,9 @@ def main():
     glc = mjkit.GameLogCrawler()
     agent = mjagent.MahjongAgent()
     gene = glc.db_get_logs_where_players_lv_gr(19)
-    glc.db_show_tables()
+    # glc.db_show_tables()
     Errors = {}
-    if path.exists('test.csv'):
+    if path.exists('test.csv'):     
         remove('test.csv')
     header = []
     for i in range(9):
@@ -29,21 +31,30 @@ def main():
             pass
         for r, sa in res.items():
             for state in sa[-1:]:
-                hand = state.s_hand34
-                row = state.s_discard34[:9]
+                random_tile = 2
+                testhand = state.s_hand34
+                testhand.append(random_tile)
+                testhand.sort()
+                discard = state.s_discard34[:9]
+                row = []
+                row.extend(discard)
+                row.append(state.s_hand34)
+                print('discard and hand:', row)
                 # waiting = waiting_calc(hand) ###TODO####
                 # draw_tile = random_tile() todo
                 waiting_tile = []
-                waiting_dict = agent.tenpai_status_check(hand)
-                for dis, wait in waiting_dict:
-                    waiting_tile.append[]
-                row.append()
-                print(row)
+                waiting_dict = agent.tenpai_status_check(testhand)
+                print('waiting_dict:', waiting_dict)
+                for dis in waiting_dict:
+                    waiting_tile.append(waiting_dict[dis])
+                row.append(waiting_tile)
+
+                print('final:', row)
                 # with open('test.csv', 'a') as csvFile:
                 #     wr = csv.writer(csvFile)
                 #     wr.writerow(row)
 
-    print('Finished Writing to CSV')
+    # print('Finished Writing to CSV')
 
 
 
