190026e8f641239af0671beeed31bba164e3b102
KoreanWienerSchnizel <joeheart1@gmail.com>
2019-11-08 15:34:56 -0800

    added a test hand

--- HandTestCases.py
+++ HandTestCases.py
@@ -1,10 +1,18 @@
+import random
+
+import MahjongKit as mjkit
+import MahjongAgent as mjagent
+
 import MahjongAgent as ma
 import Mahjong_AI as mai
 
 def main():   
     test_hands = [  [1,2,3,4,5,6,9,10,11,15,16,32,32,33],
                     [0,2,3,4,5,6,9,10,11,15,16,32,32,33],
-                    [1,2,3,1,2,3,5,6,5,6,10,12,10,12,32]    ]
+                    [1,2,3,1,2,3,5,6,5,6,10,12,10,12,32],
+                    [0,0,0,1,2,4,5,5,5,6,6,15,16,17],
+                    [0,3,4,8,10,10,15,18,19,20,20,31,33,32,]    ]
+
     agent = ma.MahjongAgent()
     ai = mai.Mahjong_AI()
     for i in test_hands:
